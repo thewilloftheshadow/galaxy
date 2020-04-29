fbf54bb79a0fafc3b4c36936036baf85925f4043
Shadow <william.shadow@outlook.com>
2020-04-29 15:18:50 -0500

    fix welcome ping...?

--- server.js
+++ server.js
@@ -204,9 +204,12 @@ client.on("message", async message => {
 client.on("guildMemberAdd", async member => {
   if(member.bot) return
   let chan = client.channels.cache.get("642214583721000972")
+  chan.send("New user has joined, generating welcome message... <a:TCKC_RainbowLoad:688544088072650821>")
   let welcome = client.guilds.cache.get(config.server).roles.cache.get("667442500029644810")
+  while(!welcome.mentionable){
   await welcome.setMentionable(true, "Welcoming " + member.user.tag)
   sleep(5000)
+  }
   chan.send(`Hey ${member}, welcome to The Cool Kids Club! 🎉\n◇────◇─────◇──◇\n<@&${welcome.id}>, please welcome them!\n◇────◇─────◇──◇\nDon’t forget to read <#642943363301244961> and <#675155024908779550>!\nAnd check out <#684203794745393199> for some custom roles!\nThen, go to <#696075558358089768> to introduce yourself!`)
   sleep(5000)  
   await welcome.setMentionable(false)
