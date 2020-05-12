6a79e22c36b5b7084fae3e8c4271d47241269a5d
Shadow <william.shadow@outlook.com>
2020-05-12 16:31:49 -0500

    change bot's status

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const cd = new db.table("cd");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`); 
-  client.user.setActivity('my DMS', { type: 'WATCHING' })
+  client.user.setActivity('everything except my DMS', { type: 'WATCHING' })
 });
 
 client.on("message", async message => {
