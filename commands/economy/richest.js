a2e55889d80af22b719241cbd71066cb02fd5e1b
Glitch (tckcgalaxy) <none>
2020-05-22 02:33:41 +0000

    😬🐚 Checkpoint ./commands/economy/whotorob.js:2449550/2251 ./commands/economy/top.js:2449550/809 ./commands/economy/richest.js:2449550/1097 ./watch.json:2449550/4

---
+++ commands/economy/richest.js
@@ -0,0 +1,17 @@
+const re = require(`../../resources.js`).data
+module.exports.run = async (client, message, args) => {
+  let lb = await re.vars.unb.getGuildLeaderboard(message.guild.id, { sort: 'cash' })
+    let m = await message.channel.send("Mirror mirror on the wall, who's the richest of them all? <a:TCKC_ThonkTriangle:678050031017918475>")
+    await re.func.sleep(5000)
+    let user = lb[Object.keys(lb)[0]]
+    m.edit(new re.Discord.MessageEmbed().setDescription(`<@${user.user_id}> is the richest user in the server. They have <a:TCKC_MoneyBag:710609208286117898> ${user.total} value.`))
+};
+
+module.exports.help = {
+  name: "richest",
+  description: "Mirror mirror on the wall, who's the richest of them all? <a:TCKC_ThonkTriangle:678050031017918475>",
+  syntax: re.config.prefix + "richest",
+  alias: [],
+  module: "economy",
+  access: {staff: false, mod: false, ecomanage: false, dev: false, owner: false}
+};
