984ec6a05111ee7f83b7562ec62805fbcb8d5842
Glitch (tckcgalaxy) <none>
2020-05-26 18:33:05 +0000

    🍼🐀 Checkpoint ./events/guildMemberRemove.js:2449550/4560 ./watch.json:2449550/4

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,18 @@
+const re = require(`../resources.js`).data;
+re.client.on("guildMemberRemove", async member => {
+  const channel = re.client.guilds.cache
+    .get(re.config.server)
+    .channels.cache.get("652530741590097930");
+  channel
+    .messages.fetch({
+      limit: 1000
+    })
+    .then(messages => {
+      messages = messages
+        .filter(m => m.author.id === member.id)
+        .array()
+        .slice(0, 1000);
+      channel.bulkDelete(messages).catch(error => console.log(error.stack));
+    });
+  re.client.emit("botlog", "Cleared messages from <#652530741590097930> from <@" + member.id + "> because they left the server")
+});
