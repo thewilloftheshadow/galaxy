a09b53a68affa7499285c241164f71ece0d7be2e
Glitch (tckcgalaxy) <none>
2020-05-26 19:02:06 +0000

    🐄💗 Checkpoint ./commands/economy/faction.js:2449550/1039

--- commands/economy/faction.js
+++ commands/economy/faction.js
@@ -1,6 +1,17 @@
 const re = require(`../../resources.js`).data
 module.exports.run = async (client, message, args) => {
-  let allfactions = re.dbs.factions.all()
+  let allfactionsdb = re.dbs.factions.all()
+  let allf = []
+  allfactionsdb.forEach(x => allf.push(x.ID))
+  let f = re.dbs.factions.get((args.join(" ") || "abcdefghijklmnopqrstuvwxyz"))
+  if(!f) return message.channel.send(`That faction was not found! Here are all the factions in the server:\n**${allf.join("**, **")}**`)
+  let embed = new re.Discord.MessageEmbed()
+  .setColor(0x30D5C8)
+  .setTitle(f.name)
+  .addField("Channel:", `<#${f.ids.channel}>`, true)
+  .addField("Role:", `<#${f.ids.role}>`, true)
+  .addField("Members:", message.guild.members.cache.filter(x => x.roles.cache.has(f.ids.role)).map(x => `<@${message.author.id}>`))
+  message.channel.send(embed)
 };
 
 module.exports.help = {
