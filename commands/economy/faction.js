10cb73945fa48ad736ce028d118e3e985b6a8024
Glitch (tckcgalaxy) <none>
2020-05-26 18:21:38 +0000

    👸🌯 Checkpoint ./commands/economy/faction.js:2449550/916

---
+++ commands/economy/faction.js
@@ -0,0 +1,17 @@
+const re = require(`../../resources.js`).data
+module.exports.run = async (client, message, args) => {
+  let lb = await re.unb.getGuildLeaderboard(message.guild.id, { sort: 'cash' })
+    let m = await message.channel.send("Let's see who has the fattest wallet... <a:TCKC_ThonkTriangle:678050031017918475>")
+    await re.func.sleep(5000)
+    let user = lb[Object.keys(lb)[0]]
+    m.edit(new re.Discord.MessageEmbed().setDescription(`I suggest you rob <@${user.user_id}>. They have <a:TCKC_MoneyBag:710609208286117898> ${user.cash} cash.`))
+};
+
+module.exports.help = {
+  name: "faction",
+  description: "Get information about a faction",
+  syntax: re.config.prefix + "faction <name>",
+  alias: [],
+  module: "economy",
+  access: {staff: false, mod: false, ecomanage: false, dev: false, owner: false}
+};
