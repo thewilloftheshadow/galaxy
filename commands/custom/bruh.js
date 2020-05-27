5d18cb0e03cc163701d77b2d714266d65a0221cf
Glitch (tckcgalaxy) <none>
2020-05-27 21:32:01 +0000

    🌃🚔 Checkpoint ./commands/custom/bruh.js:2449550/767

---
+++ commands/custom/bruh.js
@@ -0,0 +1,15 @@
+const re = require(`../../resources.js`).data
+module.exports.run = async (client, message, args) => {
+  message.delete()
+  message.channel.send("<:TCKC_LMAO:702975077825642496> Jurfan is cool")
+  re.client.emit("botlog", new re.Discord.MessageEmbed().setTitle(`Custom command \`%bestjurfan\`used by ${message.author.tag}`).setDescription(`Command used by <@${message.author.id}> in ${message.channel}`))
+};
+
+module.exports.help = {
+  name: "bruh",
+  description: "Bruh...",
+  syntax: re.config.prefix + "bruh",
+  alias: ["custom-695651396573200394"],
+  module: "custom",
+  access: {staff: false, mod: false, ecomanage: false, dev: false, owner: false}
+};
