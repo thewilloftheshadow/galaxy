f292a6b61a11d313c5570c76618e3f6aeb0be89a
Glitch (tckcgalaxy) <none>
2020-05-27 19:03:00 +0000

    🏅🐧 Checkpoint ./commands/economy/faction.js:4507627/3655

--- commands/economy/faction.js
+++ commands/economy/faction.js
@@ -8,8 +8,19 @@ module.exports.run = async (client, message, args) => {
   if(!f) return await m.edit(`That faction was not found! Here are all the factions in the server:\n**${allf.join("**, **")}**`)
   
   let fmem = message.guild.members.cache.filter(x => x.roles.cache.has(f.ids.role))
+  
+  let embed = new re.Discord.MessageEmbed()
+  .setColor(0x30D5C8)
+  .setTitle(f.name)
+  .addField("Channel:", `<#${f.ids.channel}>`, true)
+  .addField("Role:", `<@&${f.ids.role}>`, true)
+  .addField("Members:", fmem.map(x => `<@${x.user.id}>`))
+  .addField("Faction Value:", "<a:TCKC_ThonkTriangle:678050031017918475> Calculating...")
+  
+  await m.edit("Here is the information for " + f.name + ":", embed)
+  
   let ids = fmem.map(x => x.user.id)
-  let v =  0;
+  let v = 0;
   ids.forEach(async (id, index) => {
       
       let user = await re.unb.getUserBalance(m.guild.id, id);
@@ -23,32 +34,6 @@ module.exports.run = async (client, message, args) => {
         return m.edit(embed);
       }
   })
-  
-  let embed = new re.Discord.MessageEmbed()
-  .setColor(0x30D5C8)
-  .setTitle(f.name)
-  .addField("Channel:", `<#${f.ids.channel}>`, true)
-  .addField("Role:", `<@&${f.ids.role}>`, true)
-  .addField("Members:", fmem.map(x => `<@${x.user.id}>`))
-  .addField("Faction Value:", "<a:TCKC_ThonkTriangle:678050031017918475> Calculating...")
-  
-  await m.edit("Here is the information for " + f.name + ":", embed)
-  
-  async function getvalue(ids, m, v){
-    ids.forEach(async (id, index) => {
-      
-      let user = await re.unb.getUserBalance(m.guild.id, id);
-      v += user.total;
-      
-      if(index === ids.length - 1) {
-        embed.fields[embed.fields.length - 1] = {
-          name:"Faction Value:",
-          value:v
-        }
-        return m.edit(embed);
-      }
-    });
-  }
 };
 
 module.exports.help = {
