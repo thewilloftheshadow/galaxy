79f86f2833a8b4d29211ad048fe985f6dd9b0b13
Glitch (tckcgalaxy) <none>
2020-05-29 08:44:45 +0000

    🏩👓 Checkpoint ./commands/factions/adduser.js:2449550/1991 ./commands/factions/removeuser.js:2449550/1953

--- commands/factions/removeuser.js
+++ commands/factions/removeuser.js
@@ -1,7 +1,6 @@
 const re = require(`../../resources.js`).data
 module.exports.run = async (client, message, args) => {
   let m = await message.channel.send("<a:TCKC_RainbowLoad:688544088072650821>")
-  await re.func.sleep(10000)
   let f
   if(message.member.roles.cache.has("712297438014078986")) f = re.dbs.factions.get("mf")
   if(message.member.roles.cache.has("712709938161647748")) f = re.dbs.factions.get("pakistan")
@@ -9,11 +8,10 @@ module.exports.run = async (client, message, args) => {
   if(!f) return await m.edit(`You aren't in a faction!`)
   
   if(f.leader != message.author.id) return await m.edit("You aren't the leader of your faction!")
-  if(!args[0]) return await m.edit("You need to choose someone to add to your faction!")
+  if(!args[0]) return await m.edit("You need to choose someone to remove from your faction!")
   
   let user = re.func.getuser(args.join(" "), message)
   if(!user) return await m.edit("That user was not found!")
-  if(f.members.includes(user.id)) return await m.edit("That user is already in your faction!")
   
   await m.edit(`Are you sure you want to add <@${user.id}> to your faction?`)
   await m.react("678023486618468363")
