91a7aa534c587583f4aeda5d25aca47de80c5014
Glitch (tckcgalaxy) <none>
2020-05-29 07:45:34 +0000

    💴🎬 Checkpoint ./commands/factions/adduser.js:2449550/88

--- commands/factions/adduser.js
+++ commands/factions/adduser.js
@@ -3,8 +3,8 @@ module.exports.run = async (client, message, args) => {
   let m = await message.channel.send("<a:TCKC_RainbowLoad:688544088072650821>")
   let f
   if(message.member.roles.cache.has("712297438014078986")) f = re.dbs.factions.get("mf")
-  if(message.member.roles.cache.has("712297438014078986")) f = re.dbs.factions.get("mf")
-  if(message.member.roles.cache.has("712297438014078986")) f = re.dbs.factions.get("mf")
+  if(message.member.roles.cache.has("712709938161647748")) f = re.dbs.factions.get("pakistan")
+  if(message.member.roles.cache.has("712783175306444910")) f = re.dbs.factions.get("at")
   
   if(!f) return await m.edit(`You aren't in a faction!`)
   
