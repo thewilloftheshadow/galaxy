b9afd0ee2ed46b114bb8fe47fe4f774f5a53c05e
Glitch (tckcgalaxy) <none>
2020-05-29 08:33:38 +0000

    🏵🎣 Checkpoint ./commands/factions/adduser.js:2449550/364

--- commands/factions/adduser.js
+++ commands/factions/adduser.js
@@ -5,8 +5,13 @@ module.exports.run = async (client, message, args) => {
   if(message.member.roles.cache.has("712297438014078986")) f = re.dbs.factions.get("mf")
   if(message.member.roles.cache.has("712709938161647748")) f = re.dbs.factions.get("pakistan")
   if(message.member.roles.cache.has("712783175306444910")) f = re.dbs.factions.get("at")
-  
   if(!f) return await m.edit(`You aren't in a faction!`)
+  
+  if(f.leader != message.author.id) return await m.edit("You aren't the leader of your faction!")
+  if(!args[0]) return await m.edit("You need to choose someone to add to your faction!")
+  
+  let user = re.func.getuser(args.join(" "), message)
+  if(!user) return await m.edit("That user was not found!")
   
   
 };
