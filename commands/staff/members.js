5f5263f2720f27ea746fafe9b515ffe41844e847
Glitch (tckcgalaxy) <none>
2020-05-31 02:32:42 +0000

    ☄️🏫 Checkpoint ./events/guildMemberAdd.js:2449550/4448 ./watch.json:2449550/2 ./commands/staff/members.js:2449550/25

--- commands/staff/members.js
+++ commands/staff/members.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     if(!role) role = message.guild.roles.cache.get(args[0])
     if(!role) m.edit("That role doesn't exist idiot")
     let members = message.guild.members.cache.filter(m => m.roles.cache.find(r => r.name === role.name))
-    let memmsg = `${members.map(m => m.user)}`
+    let memmsg = `${members.map(m => m.user).join("\n")}`
     if(memmsg.length > 2000) memmsg = "Member list exceeded max message length"
     m.edit(memmsg, new re.Discord.MessageEmbed().setDescription("Found a total of " + members.size + " members with the " + role.name + " role"))
 };
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,16 +1,25 @@
-const re = require(`../resources.js`).data;
-// re.client.on("guildMemberAdd", async member => {
-//   if(member.bot) return
-//   if(!member.guild.id === "641036901511594003") return;
-//   let chan = re.client.channels.cache.get("642214583721000972")
-//   let m2 = await chan.send("New user has joined, generating welcome message... <a:TCKC_RainbowLoad:688544088072650821>")
-//   let welcome = re.client.guilds.cache.get(re.config.server).roles.cache.get("667442500029644810")
-//   while(!welcome.mentionable){
-//   await welcome.setMentionable(true, "Welcoming " + member.user.tag)
-//   await re.func.sleep(10000)
-//   }
-//   chan.send(`Hey ${member}, welcome to The Cool Kids Club! 🎉\n◇────◇─────◇──◇\n<@&${welcome.id}>, please welcome them!\n◇────◇─────◇──◇\nDon’t forget to read <#642943363301244961> and <#675155024908779550>!\nAnd check out <#684203794745393199> for some custom roles!\nThen, go to <#696075558358089768> to introduce yourself!`)
-//   await m2.delete()
-//   await re.func.sleep(5000)  
-//   await welcome.setMentionable(false)
-// })
+const re = require(`../resources.js`).data
+re.client.on("guildMemberAdd", async member => {
+  if (!member.bot && member.guild.id === "641036901511594003") {
+    let chan = re.client.channels.cache.get("642214583721000972")
+    let m2 = await chan.send(
+      "New user has joined, generating welcome message... <a:TCKC_RainbowLoad:688544088072650821>"
+    )
+    let welcome = re.client.guilds.cache
+      .get(re.config.server)
+      .roles.cache.get("667442500029644810")
+    while (!welcome.mentionable) {
+      await welcome.setMentionable(true, "Welcoming " + member.user.tag)
+      await re.func.sleep(10000)
+    }
+    // chan.send(
+    //   `Hey ${member}, welcome to The Cool Kids Club! 🎉\n◇────◇─────◇──◇\n<@&${welcome.id}>, please welcome them!\n◇────◇─────◇──◇\nDon’t forget to read <#642943363301244961> and <#675155024908779550>!\nAnd check out <#684203794745393199> for some custom roles!\nThen, go to <#696075558358089768> to introduce yourself!`
+    // )
+    chan.send(
+      `>>> Hey ${member}, welcome to The Cool Kids Club! <a:TCKC_Confetti:683860124888072252>\n◇────◇─────◇──◇\n<@&${welcome.id}>, please welcome them! <a:TCKC_BlobEnter:684607464225767424>\n◇────◇─────◇──◇\nFeel free to visit the following channels! <a:TCKC_CleanTick:712343103763382362>\n◇────◇─────◇──◇\n○ <#642943363301244961>\n○ <#675155024908779550>\n○ <#684203794745393199>\n○ <#696075558358089768>\n◇────◇─────◇──◇\nMake sure to boost us aswell! <a:TCKC_Nitro:683895356018130952>\n◇────◇─────◇──◇`
+    )
+    await m2.delete()
+    await re.func.sleep(5000)
+    await welcome.setMentionable(false)
+  }
+})
