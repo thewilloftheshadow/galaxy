92ca1d9fb378532859e093c7de0b2801d0c3b9b3
Glitch (tckcgalaxy) <none>
2020-05-31 22:28:23 +0000

    🛳💄 Checkpoint ./commands/war/buy.js:2449550/1247

--- commands/war/buy.js
+++ commands/war/buy.js
@@ -1,33 +1,9 @@
 const re = require(`../../resources.js`).data
 module.exports.run = async (client, message, args) => {
   
-  let items = re.dbs.users.get(message.author.id+".inventory")
-  if(!items){
-    re.dbs.users.set(message.author.id+".inventory", [])
-    items = []
-  }
-  let embeds = [new re.Discord.MessageEmbed().setTitle("Your Inventory").setAuthor(message.author.tag, message.author.avatarURL())]
+  let item = re.dbs.users.get(message.author.id+".inventory")
   
-  items.forEach(itemid => {
-    let item = JSON.parse(re.dbs.items.get(itemid))
-    if(!item.hidden){
-      if (embeds[embeds.length-1].fields.length == 6)
-        embeds.push(new re.Discord.MessageEmbed().setTitle("Your Inventory").setAuthor(message.author.tag, message.author.avatarURL()))
-      // console.log(fn.getEmoji(client, (item.emoji ? item.emoji : item.name)))
-      embeds[embeds.length - 1].addField(
-        `\`${item.id}\` - ${item.name}`, `${item.damage ? `| Damage: ${item.damage}` : ""} ${item.heal ? `| Heal: ${item.heal}` : ""} ${item.addhealth ? `| Health Boost: ${item.addhealth}` : ""}`
-      )
-      }
-    })
-    
-    for (var [i, embed] of embeds.entries()) {
-      embed.setFooter(`Page ${i + 1}/${embeds.length} | Check out the shop to get more items!\`!`)
-    }
-    
-    if(embeds[0].fields.length < 1) embeds[0].description = "There's nothing here! Go buy some items in the shop."
   
-    let m = await message.channel.send(embeds[0])
-    re.func.paginator(message.author.id, m, embeds, 0)
 };
 
 module.exports.help = {
