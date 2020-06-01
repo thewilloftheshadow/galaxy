12b1f983c81971732caa4e0932db0dead34232be
Glitch (tckcgalaxy) <none>
2020-06-01 00:00:24 +0000

    📷🚦 Checkpoint ./commands/war/buy.js:2449550/1043 ./watch.json:2449550/4 ./events/ready.js:2449550/33

--- commands/war/buy.js
+++ commands/war/buy.js
@@ -1,11 +1,32 @@
 const re = require(`../../resources.js`).data
 module.exports.run = async (client, message, args) => {
+  let m = await message.channel.send("<a:TCKC_RainbowLoad:688544088072650821>")
+  await re.func.sleep(3000)
   
   let item = re.dbs.users.get(message.author.id+".inventory")
   let user = await re.allunb[message.guild.id].getUserBalance(message.guild.id, message.author.id)
   
-  if(user.cash < item.price) return await message.channel.send(`You don't have enough cash on hand to buy this! You have ${user.cash}, but you need ${re.func.prettyNumber(item.price}`)
+  if(user.cash < item.price) return await message.channel.send(`You don't have enough cash on hand to buy this! You have ${re.func.prettyNumber(user.cash)}, but you need ${re.func.prettyNumber(item.price)}`)
   
+  await m.edit(`Are you sure you want to buy 1 ${item.name}?`)
+  await m.react("678023486618468363")
+  await m.react("684155550728192019")
+  let reactions = await m
+        .awaitReactions(
+          (r, u) =>
+            (["678023486618468363","684155550728192019"].includes(r.emoji.id)) &&
+            u.id == message.author.id,
+          { time: 30 * 1000, max: 1, errors: ["time"] }
+        )
+        .catch(() => {})
+      if (!reactions)
+        return await m.edit("Prompt timed out")
+      let reaction = reactions.first().emoji
+      await m.reactions.removeAll()
+      if (reaction.id != "678023486618468363") return await m.edit("Purchase canceled")
+  
+  
+  message.channel.send("Smh shadow didn't finish this stop trying to use it")
 };
 
 module.exports.help = {
