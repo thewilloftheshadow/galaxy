80719a1374e924ccf44d8d313da317a3536e9e88
Glitch (tckcgalaxy) <none>
2020-06-02 17:07:53 +0000

    🗼🍿 Checkpoint ./events/message.js:2449550/275 ./watch.json:2449550/4 ./commands/staff/members.js:2449550/11

--- commands/staff/members.js
+++ commands/staff/members.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     let role = message.guild.roles.cache.find(r => r.name === args.join(" "))
     if(!role) role = message.mentions.roles.first()
     if(!role) role = message.guild.roles.cache.get(args[0])
-    if(!role) m.edit("That role doesn't exist idiot")
+    if(!role) return m.edit("That role doesn't exist idiot")
     let members = message.guild.members.cache.filter(m => m.roles.cache.find(r => r.name === role.name))
     let memmsg = `${members.map(m => m.user).join("\n")}`
     if(memmsg.length > 2000) memmsg = "Member list exceeded max message length"
