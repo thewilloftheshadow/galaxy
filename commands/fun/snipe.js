b6d720f2fcfd1b27a8acd04b1d7acf801a50672f
Glitch (tckcgalaxy) <none>
2020-06-02 19:17:51 +0000

    💁🍥 Checkpoint ./commands/fun/snipe.js:2449550/1008

---
+++ commands/fun/snipe.js
@@ -0,0 +1,22 @@
+const re = require(`../../resources.js`).data
+module.exports.run = async (client, message, args) => {
+  let sniped = re.dbs.snipe.get(message.channel.id)
+  let sendsnipe = {"content": "There is nothing to snipe!", "author": re.client.user.tag}
+  if(sniped) {
+    sendsnipe.content = sniped.content
+    sendsnipe.author = sniped.author.tag
+  }
+  let embed = new re.Discord.MessageEmbed()
+  .setDescription(`Here is the last deleted message in this channel:\n\`\`\`fix\n` + sendsnipe.content + `\n\`\`\``)
+  .setTitle("Delete bad, snipe good")
+  .setAuthor(sendsnipe.author)
+  message.channel.send(embed)
+};
+
+module.exports.help = {
+  name: "snipe",
+  description: "Snipe the last deleted message in the channel",
+  syntax: re.prefix + "snipe",
+  module: "fun",
+  access: {staff: false, mod: false, ecomanage: false, dev: false, owner: false}
+};
