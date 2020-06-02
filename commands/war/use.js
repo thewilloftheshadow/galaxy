4416692e65bea50aece1c1170db5d2979e5bb18a
Glitch (tckcgalaxy) <none>
2020-06-02 18:18:27 +0000

    😘💳 Checkpoint ./commands/war/use.js:2449550/2934 ./commands/war/buy.js:2449550/64

--- commands/war/use.js
+++ commands/war/use.js
@@ -4,12 +4,14 @@ module.exports.run = async (client, message, args) => {
   await re.func.sleep(1500)
   
   let item = re.dbs.items.get(args[0])
+  let inv = re.dbs.users.get(message.author.id+".inventory")
   let user = await re.unb.getUserBalance(message.guild.id, message.author.id)
   if(!item) return m.edit("That item doesn't exist!")
+  if(!inv[item.id] || inv[item.id] < 1) return m.edit("You don't have that item!")
   
-  if(user.cash < item.price) return await m.edit(`You don't have enough cash on hand to buy this! You have ${re.func.prettyNumber(user.cash)}, but you need ${re.func.prettyNumber(item.price)}`)
+  if(item.heal == 0) return m.edit("Sorry! Only health items are avaliable to use right now")
   
-  await m.edit(`Are you sure you want to buy 1 ${item.name}?`)
+  await m.edit(`Are you sure you want to use 1 ${item.name}?`)
   await m.react("678023486618468363")
   await m.react("684155550728192019")
   let reactions = await m
@@ -24,19 +26,20 @@ module.exports.run = async (client, message, args) => {
         return await m.edit("Prompt timed out")
       let reaction = reactions.first().emoji
       await m.reactions.removeAll()
-      if (reaction.id != "678023486618468363") return await m.edit("Purchase canceled")
+      if (reaction.id != "678023486618468363") return await m.edit("Canceled")
   await m.reactions.removeAll()
   
-  re.dbs.users.add(message.author.id+".inventory."+item.id, 1)
-  re.unb.editUserBalance(message.guild.id, message.author.id, {cash: 0 - item.price}, "Purchased " + item.name)
-  await m.edit("Success! You have purchased 1 " + item.name + "!")
+  re.dbs.users.subtract(message.author.id+".inventory."+item.id, 1)
+  re.dbs.users.add(message.author.id+".hp", item.heal)
+  if(re.dbs.users.get(message.author.id+".hp") > 50) re.dbs.users.set(message.author.id+".hp", 50)
+  await m.edit(`Success! You have used 1 ${item.name} and ${item.heal ? `healed for ${item.heal} HP` : ""}!`)
 };
 
 module.exports.help = {
-  name: "buy",
-  description: "Purchase an item from the shop",
+  name: "use",
+  description: "Use an item from your inventory",
   syntax: re.config.prefix + "buy <itemid>",
-  alias: ["purchase"],
+  alias: ["heal", "equip", "attack"],
   module: "war",
   access: {staff: false, mod: false, ecomanage: false, dev: false, owner: false}
 };
