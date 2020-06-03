1414b03b875dda5d6628a5add375b33fcf1bc491
Glitch (tckcgalaxy) <none>
2020-06-03 20:12:44 +0000

    👗🚉 Checkpoint ./commands/war/buy.js:2449550/90

--- commands/war/buy.js
+++ commands/war/buy.js
@@ -3,12 +3,12 @@ module.exports.run = async (client, message, args) => {
   let m = await message.channel.send("<a:TCKC_RainbowLoad:688544088072650821>")
   await re.func.sleep(1500)
   
-  let item = re.dbs.items.get(args[0])
+  let item = re.dbs.items.get(message.guild.id+"."+args[0])
   let user = await re.unb.getUserBalance(message.guild.id, message.author.id)
   if(!item) return m.edit("That item doesn't exist!")
   
   if(user.cash < item.price) return await m.edit(`You don't have enough cash on hand to buy this! You have ${re.func.prettyNumber(user.cash)}, but you need ${re.func.prettyNumber(item.price)}`)
-  
+  console.log(item, item.name, item.price)
   await m.edit(`Are you sure you want to buy 1 ${item.name}?`)
   await m.react("678023486618468363")
   await m.react("684155550728192019")
@@ -27,7 +27,7 @@ module.exports.run = async (client, message, args) => {
       if (reaction.id != "678023486618468363") return await m.edit("Purchase canceled")
   await m.reactions.removeAll()
   
-  re.dbs.users.add(message.author.id+".inventory."+item.id, 1)
+  re.dbs.users.add(message.guild.id+"."+message.author.id+".inventory."+item.id, 1)
   re.unb.editUserBalance(message.guild.id, message.author.id, {cash: 0 - item.price}, "Purchased " + item.name)
   await m.edit("Success! You have purchased 1 " + item.name + "!")
 };
