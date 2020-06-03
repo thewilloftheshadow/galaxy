db37ab6eaba1b05cc89f12924d0af5d3cbd62c05
Glitch (tckcgalaxy) <none>
2020-06-03 09:00:44 +0000

    ⛄️🍍 Checkpoint ./commands/factions/leavefaction.js:2449550/2858 ./commands/factions/removeuser.js:2449550/15

--- commands/factions/leavefaction.js
+++ commands/factions/leavefaction.js
@@ -6,14 +6,9 @@ module.exports.run = async (client, message, args) => {
   let f = re.dbs.factions.get(message.guild.id+"."+uf)
   if(!f) return await m.edit(`You aren't in a faction!`)
   
-  if(f.leader != message.author.id) return await m.edit("You aren't the leader of your faction!")
-  if(!args[0]) return await m.edit("You need to choose someone to remove from your faction!")
   
-  let user = re.func.getuser(args.join(" "), message)
-  if(!user) return await m.edit("That user was not found!")
-  if(!f.members.includes(user.id)) return await m.edit("That user isn't in your faction!")
   
-  await m.edit(`Are you sure you want to remove <@${user.id}> from your faction?`)
+  await m.edit(`Are you sure you want to leave your faction?`)
   await m.react("678023486618468363")
   await m.react("684155550728192019")
   let reactions = await m
@@ -30,18 +25,18 @@ module.exports.run = async (client, message, args) => {
       await m.reactions.removeAll()
       if (reaction.id != "678023486618468363") return await m.edit("Ok, I won't add them then")
   
-  let newmem = re.vars.ap(f.members, user.id)
+  let newmem = re.vars.ap(f.members, message.author.id)
   re.dbs.factions.set(message.guild.id+"."+f.id+".members", newmem)
-  re.dbs.users.delete(message.guild.id+"."+user.id+".faction")
-  user.roles.remove(f.ids.role, `Removed from faction by ${message.author.tag}`)
-  await m.edit(`Done! <@${user.id}> has been removed from your faction!`)
+  re.dbs.users.delete(message.guild.id+"."+message.author.id+".faction")
+  message.member.roles.remove(f.ids.role, `User left faction`)
+  await m.edit(`<@${message.author.id}> has left their faction!`)
 };
 
 module.exports.help = {
-  name: "removeuser",
-  description: "Remove a user from your faction",
-  syntax: re.config.prefix + "removeuser <user>",
-  alias: ["removemember"],
+  name: "leavefaction",
+  description: "Leave your faction",
+  syntax: re.config.prefix + "leavefaction",
+  alias: ["leavef"],
   module: "factions",
   access: {staff: false, mod: false, ecomanage: false, dev: false, owner: false}
 };
