2311d25538f0c1e8c0e80b92d8191c4a2550f599
Glitch (tckcgalaxy) <none>
2020-06-03 10:24:44 +0000

    🐛🏁 Checkpoint ./commands/bot/setup.js:2449550/5291

--- commands/bot/setup.js
+++ commands/bot/setup.js
@@ -2,7 +2,7 @@ const re = require(`../../resources.js`).data
 module.exports.run = async (client, message, args) => {
   let m = await message.channel.send("<a:TCKC_RainbowLoad:688544088072650821>")
   await re.func.sleep(3000)
-  if(!message.member.hasPermission("MANAGE_SERVER")) return await message.channel.send("Only someone with the `Manage Server` permission can setup the bot!")
+  if(!message.member.hasPermission("MANAGE_SERVER")/* && (re.dbs.settings.get(message.guild.id+".roles.admin") && !message.member.roles.cache.has(re.dbs.settings.get(message.guild.id+".roles.admin")))*/) return await m.edit(`Only someone with the \`Manage Server\` permission /*or the <@&${re.dbs.settings.get(message.guild.id+".roles.admin")}> can setup the bot!`)
   if(re.dbs.settings.get(message.guild.id+".setup")){
     await m.edit(`Are you sure you want to rerun the setup?`)
   await m.react("678023486618468363")
@@ -38,18 +38,74 @@ module.exports.run = async (client, message, args) => {
   input = ""
   
   m.edit = await message.channel.send(
-    "Cool! Next, please ping all the roles you want considered as \"staff\" roles for this server!"
+    'Cool! Next, please ping all the roles you want considered as "staff" roles for this server, able to use command in the staff module!'
   )
-  input = await m.channel
-    .awaitMessages(msg => msg.author.id == message.author.id, {
-      time: 30 * 1000,
-      max: 1,
-      errors: ["time"]
-    })
-    .catch(() => {})
-  if (!input) return await message.author.send(m.edit("Prompt timed out."))
-  input = input.first().content
-  re.dbs.settings.set(message.guild.id+".unb.emoji", input)
+  let roles = []
+  while (!roles) {
+    input = await m.channel
+      .awaitMessages(msg => msg.author.id == message.author.id, {
+        time: 30 * 1000,
+        max: 1,
+        errors: ["time"]
+      })
+      .catch(() => {})
+    if (!input) return await message.author.send(m.edit("Prompt timed out."))
+    input = input.first()
+    if (!input.mentions.roles.first()) {
+      m.edit("Please specify at least one role!")
+    } else {
+      input.mentions.roles.forEach(x => roles.push(x.id))
+      re.dbs.settings.set(message.guild.id + ".roles.staff", roles)
+    }
+  }
+  
+  m.edit = await message.channel.send(
+    'Epic! Next, please ping all the roles you want considered as "mod" roles for this server, able to use command in the mod module!'
+  )
+  roles = []
+  while (!roles) {
+    input = await m.channel
+      .awaitMessages(msg => msg.author.id == message.author.id, {
+        time: 30 * 1000,
+        max: 1,
+        errors: ["time"]
+      })
+      .catch(() => {})
+    if (!input) return await message.author.send(m.edit("Prompt timed out."))
+    input = input.first()
+    if (!input.mentions.roles.first()) {
+      m.edit("Please specify at least one role!")
+    } else {
+      input.mentions.roles.forEach(x => roles.push(x.id))
+      re.dbs.settings.set(message.guild.id + ".roles.mod", roles)
+    }
+  }
+  
+   m.edit = await message.channel.send(
+    'Awesome! Next, please ping all the roles you want considered as "admin" roles for this server, able to manage the bot\'s settings!'
+  )
+  roles = []
+  while (!roles) {
+    input = await m.channel
+      .awaitMessages(msg => msg.author.id == message.author.id, {
+        time: 30 * 1000,
+        max: 1,
+        errors: ["time"]
+      })
+      .catch(() => {})
+    if (!input) return await message.author.send(m.edit("Prompt timed out."))
+    input = input.first()
+    if (!input.mentions.roles.first()) {
+      m.edit("Please specify at least one role!")
+    } else {
+      input.mentions.roles.forEach(x => roles.push(x.id))
+      re.dbs.settings.set(message.guild.id + ".roles.admin", roles)
+    }
+  }
+  
+   m.edit = await message.channel.send(
+    'Woohoo! You\'re all set to use the bot now!'
+  )
 }
 
 module.exports.help = {
