cea8149ce4d9d97e80cbf02e045789107dc9aca8
Glitch (tckcgalaxy) <none>
2020-06-04 03:17:30 +0000

    🐵🚣 Checkpoint ./commands/economy/whotorob.js:2449550/215 ./commands/economy/richest.js:2449550/63

--- commands/economy/richest.js
+++ commands/economy/richest.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args) => {
   let lb = await re.unb.getGuildLeaderboard(message.guild.id, { sort: 'cash' })
   let m = await message.channel.send("Mirror mirror on the wall, who's the richest of them all? <a:TCKC_ThonkTriangle:678050031017918475>")
   await re.func.sleep(5000)
-  let user = lb[Object.keys(lb)[0]]
+    let user = lb.users[0]
   m.edit(new re.Discord.MessageEmbed().setDescription(`<@${user.user_id}> is the richest user in the server. They have ${re.dbs.settings.get(message.guild.id+".unb.emoji") ? re.dbs.settings.get(message.guild.id+".unb.emoji") : "<a:TCKC_MoneyBag:710609208286117898>"}${user.total} value.`))
 };
 
--- commands/economy/whotorob.js
+++ commands/economy/whotorob.js
@@ -3,8 +3,7 @@ module.exports.run = async (client, message, args) => {
   let lb = await re.unb.getGuildLeaderboard(message.guild.id, { sort: 'cash' })
     let m = await message.channel.send("Let's see who has the fattest wallet... <a:TCKC_ThonkTriangle:678050031017918475>")
     await re.func.sleep(5000)
-    let user = lb[Object.keys(lb)[0]]
-    console.log(user, lb[Object.keys(lb)[0]])
+    let user = lb.users[0]
     m.edit(new re.Discord.MessageEmbed().setDescription(`I suggest you rob <@${user.user_id}>. They have ${re.dbs.settings.get(message.guild.id+".unb.emoji") ? re.dbs.settings.get(message.guild.id+".unb.emoji") : "<a:TCKC_MoneyBag:710609208286117898>"}${user.cash} cash.`))
 };
 
