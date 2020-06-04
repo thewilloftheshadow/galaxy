e7f0a10bcf69849751327fd23705b5333ad8389e
Glitch (tckcgalaxy) <none>
2020-06-04 01:54:36 +0000

    🛫🎏 Checkpoint ./commands/economy/whotorob.js:2449550/117

--- commands/economy/whotorob.js
+++ commands/economy/whotorob.js
@@ -3,10 +3,8 @@ module.exports.run = async (client, message, args) => {
   let lb = await re.unb.getGuildLeaderboard(message.guild.id, { sort: 'cash' })
     let m = await message.channel.send("Let's see who has the fattest wallet... <a:TCKC_ThonkTriangle:678050031017918475>")
     await re.func.sleep(5000)
-    let user = lb[0]
-    console.log(user)
-    // let user = lb[Object.keys(lb)[0]]
-    // console.log(user, Object.keys(lb.users)[0])
+    let user = lb[Object.keys(lb)[0]]
+    console.log(user, lb[Object.keys(lb)[0]])
     m.edit(new re.Discord.MessageEmbed().setDescription(`I suggest you rob <@${user.user_id}>. They have ${re.dbs.settings.get(message.guild.id+".unb.emoji") ? re.dbs.settings.get(message.guild.id+".unb.emoji") : "<a:TCKC_MoneyBag:710609208286117898>"}${user.cash} cash.`))
 };
 
