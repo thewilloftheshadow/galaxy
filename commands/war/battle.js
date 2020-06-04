d060e99766533d47ee05524a93ea7c53d5e49c40
Glitch (tckcgalaxy) <none>
2020-06-04 20:57:55 +0000

    👏🎡 Checkpoint ./commands/war/battle.js:2449550/593

--- commands/war/battle.js
+++ commands/war/battle.js
@@ -1,8 +1,13 @@
 const re = require(`../../resources.js`).data
 module.exports.run = async (client, message, args) => {
+  let m = await message.channel.send("<a:TCKC_RainbowLoad:688544088072650821>")
+  await re.func.sleep(1500)
+  
   let user1 = message.member
   let user2 = re.func.getuser(args.join(" "), message)
+  if(!user2) return await m.edit(`Unable to find that user`)
   
+  m.edit(`${user2}, ${message.author} has challenged you to a battle! Do you accept?`)
 };
 
 module.exports.help = {
