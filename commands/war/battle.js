47d4b2b0b5c0f5be5235c926099c84c0eecf3529
Glitch (tckcgalaxy) <none>
2020-06-04 21:09:55 +0000

    🌄📅 Checkpoint ./commands/war/battle.js:2449550/1393 ./watch.json:2449550/2

--- commands/war/battle.js
+++ commands/war/battle.js
@@ -4,10 +4,29 @@ module.exports.run = async (client, message, args) => {
   await re.func.sleep(1500)
   
   let user1 = message.member
+  if(!args[0]) return m.edit("You must specify someone to battle!")
   let user2 = re.func.getuser(args.join(" "), message)
   if(!user2) return await m.edit(`Unable to find that user`)
   
-  m.edit(`${user2}, ${message.author} has challenged you to a battle! Do you accept?`)
+  m.edit(`${user2}, ${message.member.displayName} has challenged you to a battle! Do you accept?`)
+  user2.send(new re.Discord.MessageEmbed().setAuthor(message.author.tag, message.author.avatarURL()).setDescription(`[${message.author.username} has challenged you to a battle!](https://discord.com/channels/${message.guild.id}/${message.channel.id}/{m.id} "Click here to accept the challenge!")`))
+  await m.react("678023486618468363")
+  await m.react("684155550728192019")
+  let reactions = await m
+        .awaitReactions(
+          (r, u) =>
+            (["678023486618468363","684155550728192019"].includes(r.emoji.id)) &&
+            u.id == user2.id,
+          { time: 30 * 1000, max: 1, errors: ["time"] }
+        )
+        .catch(() => {})
+      if (!reactions)
+        return await m.edit("Prompt timed out")
+      let reaction = reactions.first().emoji
+      await m.reactions.removeAll()
+      if (reaction.id != "678023486618468363") return await m.edit("Battle declined!")
+  await m.reactions.removeAll()
+  await m.edit("Battle accepted!")
 };
 
 module.exports.help = {
