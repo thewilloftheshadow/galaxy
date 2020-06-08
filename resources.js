3d232d4854b6ec6f1d7a22814a906d73155c5ca1
Glitch (tckcgalaxy) <none>
2020-06-08 00:02:35 +0000

    🍁💇 Checkpoint ./resources.js:2449550/1132 ./config.json:2449550/37 ./watch.json:2449550/2

--- resources.js
+++ resources.js
@@ -61,9 +61,12 @@ const func = {
       modules: [],
       eval: false
     }
+    
+    if(message.member) perms.level = 2
+    if(message.client.guilds.cache.get(config.support).members.cache.get(userid).roles.cache.has("717162873406881822")) perms.level = 3
+    if(message.client.guilds.cache.get(config.support).members.cache.get(userid).roles.cache.has("716426400738836557")) perms.level = 4
     if(userid === config.ownerID) perms.level = 5
     if(message.client.guilds.cache.get(config.support).members.cache.get(userid).roles.cache.has("719334282023272498")) perms.eval = true
-    if(message.client.guilds.cache.get(config.support).members.cache.get(userid).roles.cache.has("717162873406881822")) perms.level = 4
     
     return perms
   },
