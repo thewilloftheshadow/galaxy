08104f80738fa1f74375252fd9899e83d70be3c6
Glitch (tckcgalaxy) <none>
2020-06-09 05:48:05 +0000

    🏉🌶 Checkpoint ./bot.js:2449550/3

--- bot.js
+++ bot.js
@@ -6,9 +6,6 @@ re.client.on('error', m => console.error(m));
 re.client.on('botlog', m => re.logs.send(m))
 const server = require(`./server.js`)
 process.on('uncaughtException', error => console.error(error));
-
 re.client.commands = new re.vars.Discord.Collection()
-
 re.client.login(process.env.TOKEN);
-
 require("./handlers")(re.client);
