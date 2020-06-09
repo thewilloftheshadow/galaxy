efe76c7e653d8e26280d87a4cc86267b35e29032
Glitch (tckcgalaxy) <none>
2020-06-09 18:57:05 +0000

    🐔🚥 Checkpoint ./commands/bot/setup.js:2449550/1294

--- commands/bot/setup.js
+++ commands/bot/setup.js
@@ -7,6 +7,8 @@ module.exports.run = async (client, message, args) => {
   .setColor(re.config.color)
   .setDescription("1️⃣ Bot Settings\n2️⃣ Unbelievaboat\n3️⃣ Role Settings\n4️⃣ Module Manager Settings\n❌ Exit Settings")
   .setFooter("Shut up JC ill make better emojis and stuff later")
+  let botsettings = new re.Discord.MessageEmbed()
+  .setTitle("Bot Settings")
   await m.edit(mainmenu)
   let emojis = ["1️⃣","2️⃣","3️⃣","4️⃣","❌"]
   await m.react("1️⃣")
@@ -14,6 +16,23 @@ module.exports.run = async (client, message, args) => {
   await m.react("3️⃣")
   await m.react("4️⃣")
   await m.react("❌")
+  let reactions = await m
+        .awaitReactions(
+          (r, u) =>
+            (["1️⃣","2️⃣","3️⃣","4️⃣","❌"].includes(r.emoji)) &&
+            u.id == message.author.id,
+          { time: 120 * 1000, max: 1, errors: ["time"] }
+        )
+        .catch(() => {})
+      if (!reactions)
+        return await m.delete().then(m => message.delete)
+      let reaction = reactions.first().emoji
+      await m.reactions.removeAll()
+    if (reaction == "❌")
+        return await m.delete().then(m => message.delete)
+    if (reaction == "1️⃣"){
+      m.edit
+    }
   
 }
 
