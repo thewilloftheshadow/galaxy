b99005b4707a40ca2eeb38dfffd9624e06dc941e
Glitch (tckcgalaxy) <none>
2020-06-14 10:39:26 +0000

    🎁🍵 Checkpoint ./commands/bot/setup.js:2449550/1757 ./watch.json:2449550/4

--- commands/bot/setup.js
+++ commands/bot/setup.js
@@ -12,10 +12,10 @@ module.exports.run = async (client, message, args) => {
            `Admin Roles: ${set.roles.admin.length > 0 ? `<@&${set.roles.admin.join(">, <@&")}>` : "No roles set"}`)
   .addField("Module Manager Settings", `Economy MM: ${set.mm.economy.length > 0 ? `<@&${set.mm.economy.join(">, <@&")}>` : "No roles set"}\n` +
            `Factions MM: ${set.mm.factions.length > 0 ? `<@&${set.mm.factions.join(">, <@&")}>` : "No roles set"}\n`)
-  .addField("Channel Settings", `Galaxy Announcement Channel: ${`<#${set.channels.announcements}>` || "No channel set"}\n` +
-           `Suggestion Channel: ${`<#${set.channels.suggestions}>` || "No channel set"}\n` +
-           `1 Word Story Channel: ${`<#${set.channels.oneword ? set.channels.oneword : "No channel set"}>`}\n` +
-           `Counting Channel: ${`<#${set.channels.counting}>` || "No channel set"}\n`)
+  .addField("Channel Settings", `Galaxy Announcement Channel: ${set.channels.announcements ? `<#{set.channels.announcements}` : "No channel set"}\n` +
+           `Suggestion Channel: ${set.channels.suggestions ? `<#${set.channels.suggestions}>` : "No channel set"}\n` +
+           `1 Word Story Channel: ${`${set.channels.oneword ? `<#${set.channels.oneword}>` : "No channel set"}`}\n` +
+           `Counting Channel: ${`${set.channels.counting ? `<#${set.channels.counting}>` : "No channel set"}`}\n`)
   m.edit(" ", embed)
   } else if(args[0] === "prefix") {
     re.dbs.settings.set(message.guild.id+".prefix", args.slice(1).join(" "))
@@ -40,14 +40,31 @@ module.exports.run = async (client, message, args) => {
     let roles = input.mentions.roles.map(x => x.id)
     input.delete()
     m.edit(`Success! You have set the roles for ${type} to ${input.mentions.roles.map(x => `<@&${x.id}>`).join(", ")}`)
-    
+    re.dbs.settings.set(message.guild.id+".roles."+type, roles)
+  } else if(args[0] === "mm" || args[0] === "modulemanager") {
+    let type = args[1]
+    if(!["economy", "factions"].includes(type)) return await m.edit("That is not a valid MM setting!")
+    await m.edit("Please ping all the roles you want listed as " + type + " MMs")
+    let input = await m.channel
+    .awaitMessages(msg => msg.author.id == message.author.id, {
+      time: 30 * 1000,
+      max: 1,
+      errors: ["time"]
+    })
+    .catch(() => {})
+    if (!input) return await m.edit("Prompt timed out.")
+    input = input.first()
+    let roles = input.mentions.roles.map(x => x.id)
+    input.delete()
+    m.edit(`Success! You have set the roles for ${type} MM to ${input.mentions.roles.map(x => `<@&${x.id}>`).join(", ")}`)
+    re.dbs.settings.set(message.guild.id+".mm."+type, roles)
   }
 }
 
 module.exports.help = {
   name: `${__filename.split(`${__dirname}/`).pop().split(`.`).shift()}`,
   description: `Change or view the Galaxy Settings for your server`,
-  syntax: `${re.func.getPrefix}${__filename.split(`${__dirname}/`).pop().split(`.`).shift()}`,
+  syntax: `${re.func.getPrefix}${__filename.split(`${__dirname}/`).pop().split(`.`).shift()} <setting>`,
   alias: ["settings", "set"],
   module: `${__dirname.split(`/`).pop()}`,
   access: { level: 3, mm: null }
