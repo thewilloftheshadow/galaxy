9a8c0bf9cead414abd3bb10bdf2fad706b5862c4
Glitch (tckcgalaxy) <none>
2020-06-14 22:51:15 +0000

    🍍😎 Checkpoint ./commands/botadmin/massannounce.js:2449550/147 ./watch.json:2449550/5

--- commands/botadmin/massannounce.js
+++ commands/botadmin/massannounce.js
@@ -12,14 +12,16 @@ module.exports.run = async (client, message, args) => {
       if(guild) dms.push(guild.ownerID)
     }
   })
-  let m = await message.channel.send(`Sending message to ${channels.length} servers... <a:GS_Loading:719993838269104199>`)
+  let m = await message.channel.send(`Sending message to ${re.client.guilds.cache.size} servers... <a:GS_Loading:719993838269104199>`)
   let embed = new re.Discord.MessageEmbed().setTitle("Galaxy Announcement").setColor(re.config.color).setAuthor(message.author.tag, message.author.avatarURL()).setDescription(args.join(" ")).setFooter("Sent at").setTimestamp()
-  channels.forEach(x => {
+  channels.forEach(async x => {
     re.client.channels.cache.get(x).send(embed)
+    await re.func.sleep(1000)
   })
   embed.addField("Don't want these DMs?", `If you don't want to get announcements in your DMs, you can use the command \`%setup channel announcements\` **in your server** to set a channel for this command to be used in.`)
-  dms.forEach(x => {
-    re.client.channels.cache.get(x).send(embed)
+  dms.forEach(async x => {
+    re.client.users.cache.get(x).send(embed).catch(()=>{})
+    await re.func.sleep(1000)
   })
   m.edit(`Success! Your message was sent to ${channels.length} servers! ${dms.length} servers didn't have channels setup, so the server owner was DMed instead.`)
 };
